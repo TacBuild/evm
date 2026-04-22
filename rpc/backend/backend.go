fe0678921a261c7dad8075e8a7614c1b3f4b2993
Aleksei Kokinos <akkokinos@gmail.com>
2026-04-23 01:05:40 +0700

    fix(evm): evm chain id for APIs gets from chainConfig

--- rpc/backend/backend.go
+++ rpc/backend/backend.go
@@ -204,7 +204,7 @@ func NewBackend(
 		RPCClient:           rpcClient,
 		QueryClient:         types.NewQueryClient(clientCtx),
 		Logger:              logger.With("module", "backend"),
-		EvmChainID:          big.NewInt(int64(appConf.EVM.EVMChainID)), //nolint:gosec // G115 // won't exceed uint64
+		EvmChainID:          new(big.Int).SetUint64(evmtypes.GetChainConfig().ChainId), // derive from global chainConfig set by keeper, not app.toml
 		Cfg:                 appConf,
 		AllowUnprotectedTxs: allowUnprotectedTxs,
 		Indexer:             indexer,
